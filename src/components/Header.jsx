8aeb49dbdec91f76ccaef669207b451f97bcc4d3
Geneviève Perron Migneron <info@gen-migneron.com>
2024-01-06 18:10:48 -0500

    Add simple header

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+  return (
+    <header className="m-10 text-center">A deck merging tool for Dreamborn.ink<img src="https://dreamborn.ink/images/favicon.svg" /></header>
+  );
+}
+
+export default Header;
